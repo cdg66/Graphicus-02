ee5c0f2bfe9b977222bac0c4bd1086d22851f55a
cdg66 <claudecdg66@icloud.com>
2022-01-14 10:42:06 -0500

    modified:   couche.h

--- couche.h
+++ couche.h
@@ -1,6 +1,6 @@
 /********
  * Fichier: couche.h
- * Auteurs: C.-A. Brunet
+ * Auteurs: C.-A. Brunet C-D Gaudreault
  * Date: 08 janvier 2018 (creation)
  * Description: Declaration de la classe pour une couche dans un
  *    canevas. La classe Couche gere un vecteur de pointeur de formes
@@ -10,10 +10,25 @@
 
 #ifndef COUCHE_H
 #define COUCHE_H
+#include "Vecteur.h"
 
+#define ETATINIT    0
+#define ETATACTIF   1
+#define ETATINACTIF 2
 class Couche
 {
-   // Classe a completer
+   private:
+   int etatCouche;
+   Vecteur formes;
+   public:
+   bool setForme(Forme* forme);
+   bool removeForme(Forme* forme);
+   Forme* getForme(int index);
+   int getAire();
+   bool translaterCouche(int deltaX, int deltaY);
+   bool setEtatReinitialise();
+   bool setEtat(int Etat);
+   void afficherCouche(ostream & s); 
 };
 
 #endif
